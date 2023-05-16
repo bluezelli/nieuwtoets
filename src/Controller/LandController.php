ecd471c48fe560c19306e44779c05a7dafa275e7
bluezelli <96504071+bluezelli@users.noreply.github.com>
2023-05-16 11:19:19 +0200

    weer vastgelopen

--- src/Controller/LandController.php
+++ src/Controller/LandController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Continent;
 use App\Entity\Country;
+use App\Form\FormlandType;
 use App\Repository\CountryRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -35,21 +37,41 @@ class LandController extends AbstractController
     }
 
     #[Route('/update/{id}', name: 'refresh')]
-    public function updateshower(country $countryclass , EntityManagerInterface $entityManager , int $id): Response
+    public function updateshower( EntityManagerInterface $entityManager , Request $request, int $id): Response
     {
 //        $id = $countryclass->getId();
-        $update = $entityManager->getRepository(Country::class)->find($id);
+        $update = $entityManager->getRepository(Country::class)->findBy($id);
         $update = new Country();
-        $form = $this->createForm()
+        $form = $this->createForm(FormlandType::class, $update);
+        $form->handleRequest($request);
 
 
 
             if ($form->isSubmitted() && $form->isValid()){
-                $ = $form->getData();
-                $entityManager->persist($);
-                $entityManager->flush($);
-//            $this->redirectToRoute('home');
+              $data  = $form->getData();
+                $entityManager->persist($form);
+                $entityManager->flush($form);
+            $this->redirectToRoute('home');
             }
+        return $this->renderForm('update.html.twig', [
+            'form' => $form
+        ]);
+
+        #[Route('/update/{id}', name: 'refresh')]
+public function inserten( EntityManagerInterface $entityManager , int $id): Response
+{
+//
+        $update = new Country();
+        $form = $this->createForm(FormlandType::class, $update);
+
+
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $data  = $form->getData();
+            $entityManager->persist($);
+            $entityManager->flush($);
+//            $this->redirectToRoute('home');
+        }
         return $this->renderForm('update.html.twig', [
             'form' => $form
         ]);
